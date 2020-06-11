c1d5074a07d4a97c1d0d0692ac87424f44ace8d4
【momokan928】 <kan.kan19980928@gmail.com>
2020-06-11 15:03:20 +0900

    aas

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Person.create!(
+  name: 'てつや',
+  description: 'fさああ',
+  id: 1
+  )
+Person.create!(
+  name: 'てつや',
+  description: 'fさああ',
+  id: 2
+  )
+Person.create!(
+  name: 'てつや',
+  description: 'fさああ',
+  id: 3
+  )
+Person.create!(
+  name: 'てつや',
+  description: 'fさああ',
+  id: 4
+  )
+Person.create!(
+  name: 'てつや',
+  description: 'fさああ',
+  id: 5
+  )
+Person.create!(
+  name: 'てつや',
+  description: 'fさああ',
+  id: 6
+  )
