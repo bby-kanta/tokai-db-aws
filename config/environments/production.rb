3d46a643a2ebfc792973ac5decc25b59aeb7d5d3
【momokan928】 <kan.kan19980928@gmail.com>
2020-06-11 11:30:17 +0900

    production.rb を編集

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,14 +20,12 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = true
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.css_compressor = :sass
-  config.assets.enabled = true
   config.assets.compile = true
   config.assets.initialize_on_precompile=false
 
