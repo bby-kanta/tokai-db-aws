888e1dcb72557a4f4a7b7ccc3ec82b2af25f7d02
【momokan928】 <kan.kan19980928@gmail.com>
2020-06-21 20:49:25 +0900

    font-awesomeでいいねボタン。全動画一覧以外のタブでajaxが動かない

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'relationships/create'
   get 'relationships/destroy'
   devise_for :users
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :videos do
     resource :relationships,only: [:create,:destroy]
