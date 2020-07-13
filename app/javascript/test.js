ed645cdf4c33ea5db296136ad6a68de2100e3f3d
Kanta <kan.kan19980928@gmail.com>
2020-07-13 21:13:54 +0900

    見所に時間指定リンクを書けるようにした

--- app/javascript/test.js
+++ app/javascript/test.js
@@ -1,5 +1,8 @@
-// $(function() {
-    
-//   $(".S").css("color", "red");
-//   console.log("huga")
-// });
+//一番上のページにスクロールする
+$(function() {
+  // 「TOPに戻る」ボタンがクリックされた時の動きを指定します。
+  $("#click").click(function() {
+    // ページの一番上までスクロールさせます。
+    $('body, html').scrollTop(0);
+  });
+});
