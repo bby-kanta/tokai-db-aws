1ad6df879ad71ce972670900032cb0ef24e982b6
Kanta <kan.kan19980928@gmail.com>
2020-07-13 21:55:30 +0900

    セレクタに[id=]と書けば同じIDが複数あっても全て取得できる

--- app/javascript/test.js
+++ app/javascript/test.js
@@ -1,7 +1,7 @@
 //一番上のページにスクロールする
 $(function() {
   // 「TOPに戻る」ボタンがクリックされた時の動きを指定します。
-  $("#click").click(function() {
+  $("[id=click]").click(function() {
     // ページの一番上までスクロールさせます。
     $('body, html').scrollTop(0);
   });
