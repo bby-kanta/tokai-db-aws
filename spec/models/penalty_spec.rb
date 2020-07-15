c737cf01f9a1300fd5a488d69a2a21df9cafefc6
Kanta <kan.kan19980928@gmail.com>
2020-07-15 23:08:26 +0900

    アソシエーションのテストもできた

--- spec/models/penalty_spec.rb
+++ spec/models/penalty_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Penalty, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do 
+    @penalty = FactoryBot.build(:penalty)
+  end
+
+  describe 'バリデーション' do
+    it 'それぞれ値が設定されていれば、OK' do
+      expect(@penalty.valid?).to eq(true)
+    end
+  end
+
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do 
+    @tag = FactoryBot.build(:tag)
+  end
+
+  describe 'バリデーション' do
+    it 'それぞれ値が設定されていれば、OK' do
+      expect(@tag.valid?).to eq(true)
+    end
+  end
+
 end
