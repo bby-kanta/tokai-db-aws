f4508ec27d68b4f20162dc93e6323b6a93df3628
Kanta <kan.kan19980928@gmail.com>
2020-07-22 17:21:33 +0900

    アナリティクス用の設定を追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -115,7 +115,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  GA.tracker = "UA-170820715-1"
+  GA.tracker = "UA-170820715-2"
 
 
 end
